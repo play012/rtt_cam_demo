989ee6ed1803e103cb3f56992251dc63c11c7627
play012 <s.friesen1@protonmail.com>
2022-08-19 01:42:23 +0200

    Updates

---
+++ precache-manifest.eddaed5f05ebb26eada57ae6d33bad71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "98524eb6ef4396b4b40f",
+    "url": "/rtt_cam_demo/static/js/main.02ce2104.chunk.js"
+  },
+  {
+    "revision": "e74740b260099027250c",
+    "url": "/rtt_cam_demo/static/js/runtime~main.c415e7a4.js"
+  },
+  {
+    "revision": "93c21d975ef6ef941666",
+    "url": "/rtt_cam_demo/static/js/2.ec8e9be9.chunk.js"
+  },
+  {
+    "revision": "3f7bd488fdaed15ee1d511b61a3f26e4",
+    "url": "/rtt_cam_demo/index.html"
+  }
+];
